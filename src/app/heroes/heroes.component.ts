b3bdb58464fc4168ad7bfc0fcbf5e8d918e83711
Anton Semenov <anseme@gmail.com>
2020-05-30 12:54:38 +0200

    Connect HeroComponent and HeroesService

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,14 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../services/heroes.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
 })
 export class HeroesComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private heroesService: HeroesService) {
+    console.log('HeroesComponent contructor');
   }
 
+  ngOnInit(): void {
+    console.log('HeroesComponent ngOnInit');
+  }
 }
