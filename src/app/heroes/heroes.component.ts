f1f9e568526ae3788a984bc9cae16e2be67a3c3c
Anton Semenov <anseme@gmail.com>
2020-05-30 13:39:58 +0200

    connect type Hero and prepare variable heroes to work

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,16 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import { HeroesService } from '../services/heroes.service';
+import { HeroesService, Hero } from '../services/heroes.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
 })
 export class HeroesComponent implements OnInit {
+  heroes: Hero[] = [];
   constructor(private heroesService: HeroesService) {
+    console.log(heroesService);
     console.log('HeroesComponent contructor');
   }
 
   ngOnInit(): void {
+    this.heroes = this.heroesService.getHeroes();
+    console.log(this.heroes);
     console.log('HeroesComponent ngOnInit');
   }
 }
